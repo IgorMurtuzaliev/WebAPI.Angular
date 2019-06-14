92051e14cc10a51ba0560d4fd5a70d5c6b556679
i.murtuzaliev <ingwarrior.99@yandex.ru>
2019-06-14 19:33:37 +0300

    commit home

--- src/app/home/page/users-contacts/edit-contact/edit-contact.component.ts
+++ src/app/home/page/users-contacts/edit-contact/edit-contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { ContactService } from '../../../shared/contact.service'
+import { ContactsComponent } from 'src/app/home/contacts/contacts.component';
 @Component({
   selector: 'app-edit-contact',
   templateUrl: './edit-contact.component.html',
@@ -7,13 +8,14 @@ import { ContactService } from '../../../shared/contact.service'
 })
 export class EditContactComponent implements OnInit {
   @Input() contact:any;
-  constructor(private service: ContactService) { }
+  constructor(private service: ContactService ,private contacts:ContactsComponent) { }
 
   ngOnInit() {
   }
   onEdit(Id) {
     this.service.editContactName(Id).subscribe(
       res => {
+this.contacts.ngOnInit();
       },
       err => {
         console.log(err);
